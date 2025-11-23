c6ce0ee29da34e1d6d19edc615a8ab46415b53e7
Marc Röthlisberger <marc.roethlisberger@digitecgalaxus.ch>
2025-11-23 13:44:07 +0100

    Added first scaffold to open a TCP socket with known IP

--- ESP_Home_interactor/Program.cs
+++ ESP_Home_interactor/Program.cs
@@ -1,27 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Sockets;
 
-Console.WriteLine("Hello, World!");
+var host = "192.168.0.26";
+var port = 6053;
 
-/*
- async def main():
-       """Connect to an ESPHome device and wait for state changes."""
-       cli = aioesphomeapi.APIClient("api_test.local", 6053, "MyPassword")
-   
-       await cli.connect(login=True)
-   
-       def change_callback(state):
-           """Print the state changes of the device.."""
-           print(state)
-   
-       # Subscribe to the state changes
-       cli.subscribe_states(change_callback)
-   
-   loop = asyncio.get_event_loop()
-   try:
-       asyncio.ensure_future(main())
-       loop.run_forever()
-   except KeyboardInterrupt:
-       pass
-   finally:
-       loop.close() 
-*/
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+await socket.ConnectAsync(host, port);
+var isSocketConnected = socket.Connected;
+
+var stream = new NetworkStream(socket);
+
+Console.WriteLine($"Connected to {host}:{port}");
+
+// Keep connection open
+Console.ReadLine();
+
+socket.Close();
