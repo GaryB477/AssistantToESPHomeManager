ecdce2aca4ef78e97ec66ba78d2fedfbe88a87bf
Marc Röthlisberger <marc.roethlisberger@digitecgalaxus.ch>
2026-01-11 14:49:29 +0100

    cleanup

--- ESP_Home_interactor/Program.cs
+++ ESP_Home_interactor/Program.cs
@@ -1,8 +1,5 @@
-using System.Runtime.InteropServices;
 using ESP_Home_Interactor;
 using ESP_Home_Interactor.Config;
-using ESP_Home_Interactor.Entities;
-using ESP_Home_Interactor.helper;
 
 var espConfigs = await Config.Read("/Users/marc/git/private/HomePeter/ESP_Home_interactor/config.json");
 var ESPs = espConfigs.ESPNode.Select(esp => new EspBase(esp)).ToList();
